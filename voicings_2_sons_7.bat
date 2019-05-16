6467b6b6d51ee66eadf6b9fda20759dd228784cc
hugohouel <hugoh951@hotmail.com>
2019-05-16 20:42:36 +0200

    working on the voicings

---
+++ voicings_2_sons_7.bat
@@ -0,0 +1,2 @@
+:: Accord7 2 sons en position basse puis haute
+python train.py --mode 4 --delta 2.7 --nb_cycles 2
---
+++ voicings_2_sons_min7.bat
@@ -0,0 +1,2 @@
+:: Min7 2 sons en position basse puis haute
+python train.py --mode 3 --delta 3.5 --nb_cycles 2
