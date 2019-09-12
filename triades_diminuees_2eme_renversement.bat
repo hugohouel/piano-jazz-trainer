b242bdd147100fdd42bd350b7f17da44c1f0a929
hugohouel <hugoh951@hotmail.com>
2019-09-12 08:13:43 +0200

    the grove or nothing

--- triades_diminuees_2eme_renversement.bat
+++ triades_diminuees_2eme_renversement.bat
@@ -1,4 +1,4 @@
 ﻿:: Triade diminuée position 5 1 3
 :: Mettre l'index sur la tonique; on a ensuite la tierce mineure à jouer à droite et le triton à gauche avec le 5ème doigt.
 
-python train.py --mode 7 --delta 2.9 --nb_cycles 2 -t "5 1 3"
+python train.py --mode 7 --delta 2.6 --nb_cycles 2 -t "5 1 3"
--- voicing_maj7_5_7_1_3.bat
+++ voicing_maj7_5_7_1_3.bat
@@ -1,3 +1,3 @@
 :: Maj7 en position 5 7 1 3
 
-python train.py --mode 5 --delta 2.0 --nb_cycles 2 --text "5 7 1 3"
+python train.py --mode 5 --delta 1.8 --nb_cycles 2 --text "5 7 1 3"
