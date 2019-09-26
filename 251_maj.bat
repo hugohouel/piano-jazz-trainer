cfd0dc008344d3c2b640f80deeb01377cb68fd45
hugohouel <hugoh951@hotmail.com>
2019-09-26 08:13:15 +0200

    new files

---
+++ 251_maj.bat
@@ -0,0 +1,4 @@
+﻿:: 2 5 1 majeurs
+:: record 5
+
+python train.py --mode 1 --delta 14 --nb_cycles 1 -t "2 5 1 majeurs"
---
+++ voicings_maj7_7_9_3_5.bat
@@ -0,0 +1,3 @@
+:: Maj7 en position 7 9 3 5. Record 2.2
+
+python train.py --mode 5 --delta 2.0 --nb_cycles 2 --text "7 9 3 5"
