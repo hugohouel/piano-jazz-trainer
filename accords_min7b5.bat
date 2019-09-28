08751d5586c0439da8463180242602d13c3c73a9
hugohouel <hugoh951@hotmail.com>
2019-09-28 15:58:54 +0200

    addind min7b5 et amure

---
+++ accords_min7b5.bat
@@ -0,0 +1,4 @@
+﻿:: Je joue les accords demi-dim - ou min7b5 en position fondamentale 1 3 5 7
+:: Record 5
+
+python train.py --mode 1 --delta 5 --nb_cycles 1
---
+++ armure.bat
@@ -0,0 +1,4 @@
+﻿:: Je donne l'armure de la tonalité majeure
+:: Record 6
+
+python train.py --mode 1 --delta 6 --nb_cycles 1
