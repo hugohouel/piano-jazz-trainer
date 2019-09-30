08e71fb3bc355ce18bac8427dde7117a38cc6484
hugohouel <hugoh951@hotmail.com>
2019-09-30 08:13:10 +0200

    improving

--- 251_maj.bat
+++ 251_maj.bat
@@ -1,4 +1,4 @@
 ﻿:: 2 5 1 majeurs. Faire les 2 positions, sauf pour Fa# où il n'y en a qu'une.
-:: record 10
+:: record 8.3
 
-python train.py --mode 1 --delta 8.5 --nb_cycles 1 -t "2 5 1 majeurs"
+python train.py --mode 1 --delta 8.1 --nb_cycles 2 -t "2 5 1 majeurs"
--- accords_min7b5.bat
+++ accords_min7b5.bat
@@ -1,4 +1,4 @@
 ﻿:: Accords demi-dim - ou min7b5 en position fondamentale 1 3 5 7. Jouer la triade dim et ajouter la 7 mineure
-:: Record 6
+:: Record 4.5
 
-python train.py --mode 1 --delta 5.5 --nb_cycles 1
+python train.py --mode 6 --delta 4.0 --nb_cycles 2
