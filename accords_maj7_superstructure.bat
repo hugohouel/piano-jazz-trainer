744b2e225c56ad8f9ab0969e79f95c60eb7ca193
hugohouel <hugoh951@hotmail.com>
2019-10-01 08:04:42 +0200

    improving and reorganizing

--- accords_maj7_superstructure.bat
+++ accords_maj7_superstructure.bat
@@ -1,4 +1,4 @@
 ﻿:: Accords demi-dim - ou min7b5 en position fondamentale 1 3 5 7. Jouer la triade dim et ajouter la 7 mineure
 :: Record 4.0
 
-python train.py --mode 6 --delta 3.5 --nb_cycles 2
+python train.py --mode 6 --delta 3.7 --nb_cycles 2
--- modes.bat
+++ modes.bat
@@ -1,3 +1,5 @@
 ﻿:: Modes majeurs
+:: Record 20 (pendant 4 minutes)
+:: Avec delta = 20 secondes, il faut 40 minutes pour tous les faire!
 
 python train.py --mode 9 --delta 20 --nb_cycles 1
