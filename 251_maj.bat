ff3f3f46eaf910a8c82d5e478304e9fb87af0690
hugohouel <hugoh951@hotmail.com>
2019-10-02 08:04:15 +0200

    improving greatly

--- 251_maj.bat
+++ 251_maj.bat
@@ -1,4 +1,4 @@
 ﻿:: 2 5 1 majeurs. Faire les 2 positions, sauf pour Fa# où il n'y en a qu'une.
-:: record 8.3
+:: record 7.7
 
-python train.py --mode 1 --delta 8.1 --nb_cycles 2 -t "2 5 1 majeurs"
+python train.py --mode 1 --delta 7.5 --nb_cycles 2 -t "2 5 1 majeurs"
