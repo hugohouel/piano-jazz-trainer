9bb4d334a7d39165c17cd99692fbe5fe152259d5
hugohouel <hugoh951@hotmail.com>
2019-10-10 22:27:39 +0200

    more

--- modes.bat
+++ modes.bat
@@ -2,4 +2,4 @@
 :: Record 20 (pendant 4 minutes)
 :: Avec delta = 20 secondes, il faut 40 minutes pour tous les faire! (119 = 17 * 7 positions) Avec 12.5 sec, 24 minutes.
 
-python train.py --mode 9 --delta 12.5 --nb_cycles 1
+python train.py --mode 9 --delta 12.0 --nb_cycles 1
