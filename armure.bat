51a04f6421a2b652e057094871cac15796880c29
hugohouel <hugoh951@hotmail.com>
2019-10-13 11:00:53 +0200

    more

--- armure.bat
+++ armure.bat
@@ -1,4 +1,4 @@
 ﻿:: Je donne l'armure de la tonalité majeure
-:: Record 4.0
+:: Record 2.6
 
-python train.py --mode 1 --delta 3.5 --nb_cycles 1
+python train.py --mode 1 --delta 2.4 --nb_cycles 1
--- modes.bat
+++ modes.bat
@@ -1,5 +1,6 @@
 ﻿:: Modes majeurs
-:: Record 20 (pendant 4 minutes)
+:: Plaquer les degrés 1 3 5 7 main gauche et la superstructure à la main droite, mélodiquement.
+:: Record 11
 :: Avec delta = x secondes, il faut 2x minutes pour tout faire (119 = 17 * 7 pos.)
 
-python train.py --mode 9 --delta 11 --nb_cycles 1
+python train.py --mode 9 --delta 10.9 --nb_cycles 1
