c5823c469f39da7d29451ee0b1f76a52e8307139
hugohouel <hugoh951@hotmail.com>
2019-10-20 11:34:31 +0200

    more

--- modes.bat
+++ modes.bat
@@ -3,4 +3,4 @@
 :: Record 10
 :: Avec delta = x secondes, il faut 2x minutes pour tout faire (119 = 17 * 7 pos.)
 
-python train.py --mode 9 --delta 9.5 --nb_cycles 1
+python train.py --mode 9 --delta 9.3 --nb_cycles 1
