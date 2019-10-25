f0abf9fa5c8c5ebb7ce9774df0aca378572aed94
hugohouel <hugoh951@hotmail.com>
2019-10-25 08:27:30 +0200

    more

---
+++ 251_maj_alteres_1pos_maj7_7935.bat
@@ -0,0 +1,7 @@
+﻿:: 2 5 1 majeurs avec altération sur l'accord 7 du 5ème degré. 
+:: Faire les 2 positions, sauf pour Fa# où il n'y en a qu'une.
+:: On altère, sur le 5, la note la plus haute : on a donc soit b9 soit b13.
+
+:: record 6.8
+
+python train.py --mode 1 --delta 6.7 --nb_cycles 2 -t "2 5 1 majeurs alteres"
