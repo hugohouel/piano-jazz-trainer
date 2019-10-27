0381c78b3bf0bd4402f7a4c8fcf1157fef721268
hugohouel <hugoh951@hotmail.com>
2019-10-27 09:06:57 +0100

    more

--- 251_maj_alteres_1pos_maj7_3579.bat
+++ 251_maj_alteres_1pos_maj7_3579.bat
@@ -1,7 +1,7 @@
 ﻿:: 2 5 1 majeurs avec altération sur l'accord 7 du 5ème degré. 
-:: Faire les 2 positions, sauf pour Fa# où il n'y en a qu'une.
-:: On altère, sur le 5, la note la plus haute : on a donc soit b9 soit b13.
+:: Faire 1 position, celle qui commence par le min7 de type I (tierce en bas, septième en haut), position aérée.
+:: On altère la 13 en b13.
 
-:: record 6.8
+:: record 6.7
 
 python train.py --mode 1 --delta 6.7 --nb_cycles 2 -t "2 5 1 majeurs alteres"
--- 251_maj_alteres_1pos_maj7_7935.bat
+++ 251_maj_alteres_1pos_maj7_7935.bat
@@ -1,7 +1,7 @@
 ﻿:: 2 5 1 majeurs avec altération sur l'accord 7 du 5ème degré. 
-:: Faire les 2 positions, sauf pour Fa# où il n'y en a qu'une.
-:: On altère, sur le 5, la note la plus haute : on a donc soit b9 soit b13.
+:: Faire 1 positions, celle qui commence par le voicing min7 de type 2 (serré) avec septième en bas et tierce en haut.
+:: On altère la 9 en b9.
 
-:: record 6.8
+:: record 6.7
 
-python train.py --mode 1 --delta 6.7 --nb_cycles 2 -t "2 5 1 majeurs alteres"
+python train.py --mode 1 --delta 6.0 --nb_cycles 2 -t "2 5 1 majeurs alteres"
