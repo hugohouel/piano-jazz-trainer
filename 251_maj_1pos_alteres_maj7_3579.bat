0daf53bb960c49500b0d46b26963194541ef7d18
hugohouel <hugoh951@hotmail.com>
2019-10-31 08:23:11 +0100

    more

--- 251_maj_1pos_alteres_maj7_3579.bat
+++ 251_maj_1pos_alteres_maj7_3579.bat
@@ -2,6 +2,6 @@
 :: Faire 1 position, celle qui commence par le min7 de type I (tierce en bas, septième en haut), position aérée.
 :: On altère la 13 en b13.
 
-:: record 5.0
+:: record 4.6
 
-python train.py --mode 1 --delta 4.6 --nb_cycles 2 -t "2 5 1 majeurs alteres"
+python train.py --mode 1 --delta 4.4 --nb_cycles 2 -t "2 5 1 majeurs alteres"
--- intervalles_tritons.bat
+++ intervalles_tritons.bat
@@ -1,4 +1,7 @@
 ﻿:: Tritons
-:: Record 7
+:: Dire le triton à voix haute
+:: Record 6.5
 
-python train.py --mode 991 --delta 7.0 --nb_cycles 1
+:: RQ. Les tritons sont symétriques : le triton de C est F# et réciproquement. 
+
+python train.py --mode 991 --delta 6.0 --nb_cycles 1
