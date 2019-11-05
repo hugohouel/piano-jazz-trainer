9a1ae80acc6910ffa6a949d737123b26312f0d55
hugohouel <hugoh951@hotmail.com>
2019-11-05 08:04:54 +0100

    more

--- intervalles_tritons.bat
+++ intervalles_tritons.bat
@@ -4,4 +4,4 @@
 
 :: RQ. Les tritons sont symétriques car 2 tritons font une octave : le triton de C est F# et réciproquement. 
 
-python train.py --mode 991 --delta 5.0 --nb_cycles 1
+python train.py --mode 991 --delta 3.7 --nb_cycles 1
