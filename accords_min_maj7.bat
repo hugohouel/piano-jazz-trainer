c880e169886f6b3e028d7eeadbcd58e3e874d7a1
hugohouel <hugoh951@hotmail.com>
2019-11-15 08:53:04 +0100

    more

---
+++ accords_min_maj7.bat
@@ -0,0 +1,6 @@
+﻿:: Accords mineurs maj7 en position fondamentale 1 3 5 7. Jouer la triade mineure et ajouter la 7 majeure
+:: Record 7
+
+:: A coder dans pycharm!
+
+python train.py --mode 6 --delta 2.5 --nb_cycles 2
---
+++ voicings_min7b5_7935.bat
@@ -0,0 +1,4 @@
+﻿:: Accords demi-dim - ou min7b5 en position fondamentale 1 3 5 7. Jouer la triade dim et ajouter la 7 mineure
+:: Record 2.9
+
+python train.py --mode 6 --delta 2.5 --nb_cycles 2
