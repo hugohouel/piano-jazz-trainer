fcfd2fff5a226d67d56ae2daaf84d8cfb47f85b1
hugo <hugog951@hotmail.com>
2019-11-26 08:31:29 +0100

    more

--- voicings_min_maj7_2_sons.bat
+++ voicings_min_maj7_2_sons.bat
@@ -1,3 +1,3 @@
 :: Voicings de type I et II pour min maj7 à 2 sons
 
-python train.py --mode 0 --delta 4.2 --nb_cycles 2
+python train.py --mode 0 --delta 3.3 --nb_cycles 2
--- voicings_min_maj7_3_sons.bat
+++ voicings_min_maj7_3_sons.bat
@@ -1,4 +1,6 @@
 :: Voicings min maj7 à 3 sons de type I et II.
 :: Ca marche comme pour min7. On joue type I : 379, type II : 735.
+:: Les voicings min_maj7 3 sons de type II sont des triades augmentées en partant de la sensible.
+
 
 python train.py --mode 0 --delta 11 --nb_cycles 2
