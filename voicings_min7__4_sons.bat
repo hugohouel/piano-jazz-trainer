c7b52eaa25a905ba7891893b6dec3b9a62245b5b
hugo <hugog951@hotmail.com>
2019-11-27 09:00:25 +0100

    more

--- voicings_min7__4_sons.bat
+++ voicings_min7__4_sons.bat
@@ -1,4 +1,4 @@
 :: Min7 4 sons type I et type II.
 :: Les voicings min7 à 4 sons de type I sont les accords maj7 en position 1357 de la b3.
 
-python train.py --mode 3 --delta 7.0 --nb_cycles 2
+python train.py --mode 3 --delta 5.5 --nb_cycles 1
