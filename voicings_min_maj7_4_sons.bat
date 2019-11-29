db18292d6a06ca4f592bd452cf04562ff8e1c53e
hugo <hugog951@hotmail.com>
2019-11-29 08:25:58 +0100

    more

--- voicings_min_maj7_4_sons.bat
+++ voicings_min_maj7_4_sons.bat
@@ -1,6 +1,5 @@
 :: Voicings min maj7 4 sons en position type I et type II.
 :: Les voicings min maj7 4 sons de type I sont des accords maj7 #5 une tierce mineure au dessus du I.
-:: PAS ENCORE FAIT
 
 
-python train.py --mode 0 --delta 14 --nb_cycles 1
+python train.py --mode 0 --delta 7 --nb_cycles 1
