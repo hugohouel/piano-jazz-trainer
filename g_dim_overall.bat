3ba5dadd76df8743c4fcfe85f1303685eceb72ad
hugohouel <hugog951@hotmail.com>
2021-04-18 10:50:48 +0200

    fix

--- g_dim_overall.bat
+++ g_dim_overall.bat
@@ -1,4 +1,5 @@
 :: Je donne la gamme diminuée (C, F, G) à laquelle correspond la gamme demandée. Ex : Eb correspond à la même gamme que C dim.
+:: Astuce : La note Y correspond à la gamme du ton X si : elle est une tierce mineur dessus, dessous, ou est son triton.
 :: Record 5
 
-python train.py --mode 7 --delta 4 --nb_cycles 1 -s 5
+python train.py --mode 7 --delta 3.5 --nb_cycles 1 -s 5
